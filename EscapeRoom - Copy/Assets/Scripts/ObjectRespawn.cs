03e5adc9f9889d3c3d6cb7034ea5f1ddb1810c29
ChrisInYellow <31647524+ChrisInYellow@users.noreply.github.com>
2018-02-05 16:01:46 +0100

    Balls

--- EscapeRoom - Copy/Assets/Scripts/ObjectRespawn.cs
+++ EscapeRoom - Copy/Assets/Scripts/ObjectRespawn.cs
@@ -2,25 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectRespawn : MonoBehaviour {
+public class ObjectRespawn : MonoBehaviour
+{
 
     private Transform startPosition;
-    private Vector3 storageVector; 
-    private GameObject self; 
-	// Use this for initialization
-	void Start () {
+    private Vector3 storageVector;
+    private GameObject self;
+    private GameObject respawner; 
+    // Use this for initialization
+    void Start()
+    {
 
-        startPosition = transform; 
-	}
+        startPosition = transform;
+    }
 
     public void OnTriggerEnter(Collider other)
     {
-        Instantiate(transform.gameObject, startPosition); 
-        Destroy(gameObject); 
+        Instantiate(transform.gameObject, startPosition);
+        Destroy(gameObject);
 
     }
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
