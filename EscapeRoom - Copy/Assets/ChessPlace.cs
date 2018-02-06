4c343d0d3190e05cc5822cd31a5526db690a76ee
Katomic88 <31647167+Katomic88@users.noreply.github.com>
2018-02-06 16:06:17 +0100

    Chess basically damn done
    yes boi!

---
+++ EscapeRoom - Copy/Assets/ChessPlace.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessPlace : MonoBehaviour {
+
+    public void SendInfo()
+    {
+        GetComponentInParent<ChessCorrectCombo>().Test(gameObject, transform.GetChild(1).gameObject);
+    }
+
+}
