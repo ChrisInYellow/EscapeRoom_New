604abbcd39dea38cab60bc2ee024cd6b858d8300
ChrisInYellow <31647524+ChrisInYellow@users.noreply.github.com>
2018-02-08 11:41:59 +0100

    Balls

---
+++ EscapeRoom - Copy/Assets/Scripts/LaserSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSpawn : MonoBehaviour {
+
+    //public LineRenderer laser;
+    public Vector3 amountOfDegrees; 
+    public float speed; 
+    private Quaternion laserRotation; 
+    
+
+	// Use this for initialization
+	void Start () {
+
+         gameObject.transform.Rotate(Vector3.zero); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RealignLaser()
+    {
+        gameObject.transform.Rotate(amountOfDegrees); 
+    }
+}
