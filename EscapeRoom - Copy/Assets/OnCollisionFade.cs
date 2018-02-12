d03cef85c71422e4d6a3657af2bc1f495295cbd9
Hugo Cremer <slasken49@gmail.com>
2018-02-12 15:50:53 +0100

    Commit

---
+++ EscapeRoom - Copy/Assets/OnCollisionFade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class OnCollisionFade : MonoBehaviour 
+{
+    public VRTK_HeadsetFade headSetFade;
+    void Start () 
+	{
+
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Niklas");
+        Debug.Log(other.tag);
+        if (other.tag == "Wall")
+        {
+            Debug.Log("WTF");
+            headSetFade.Fade(new Color(0, 0, 0), 1f);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Wall")
+        {
+            headSetFade.Unfade(1f);
+        }
+    }
+}
