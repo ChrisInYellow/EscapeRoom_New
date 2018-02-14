89746da7d649be96c6a3cb86368b92e0399bf724
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2018-02-14 09:20:59 +0100

    Eventthingy added to doorslam

--- EscapeRoom - Copy/Assets/DoorSlamScript.cs
+++ EscapeRoom - Copy/Assets/DoorSlamScript.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DoorSlamScript : MonoBehaviour {
 
+    public UnityEvent DoorSlamed = new UnityEvent();
+    public bool doorslamed;
     public GameObject leftDoor;
     public GameObject rightDoor;
 
     private void OnTriggerEnter(Collider other)
     {
+        DoorSlamActivate();
+    }
+    public void DoorSlamActivate()
+    {
         leftDoor.GetComponent<Animator>().SetTrigger("DoorSlam");
         rightDoor.GetComponent<Animator>().SetTrigger("DoorSlam");
+        DoorSlamed.Invoke();
         Destroy(gameObject);
     }
 }
