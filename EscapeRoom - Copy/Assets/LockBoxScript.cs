52ba9efb1b98127491885cf97156b6e9c7462120
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2018-02-14 14:19:52 +0100

    Stuff

--- EscapeRoom - Copy/Assets/LockBoxScript.cs
+++ EscapeRoom - Copy/Assets/LockBoxScript.cs
@@ -17,19 +17,23 @@ public class LockBoxScript : MonoBehaviour {
 	
 	
 	void Update () {
-		if(codeNumber == CodeCorrect.One && gameObject.transform.rotation.x <= 50 && gameObject.transform.rotation.x >= 75)
+        if(gameObject.transform.rotation.x <= 450 || gameObject.transform.rotation.x >= 270)
+        {
+            gameObject.transform.rotation = new Quaternion (-0.707f,0,0,0.707f);
+        }
+		else if(codeNumber == CodeCorrect.One && gameObject.transform.rotation.x <= 252 && gameObject.transform.rotation.x >= 216 || gameObject.transform.rotation.x <= -396 && gameObject.transform.rotation.x >= -432)
         {
             spinnerOne = true;
         }
-        if (codeNumber == CodeCorrect.Two && gameObject.transform.rotation.x <= 50 && gameObject.transform.rotation.x >= 75)
+        else if (codeNumber == CodeCorrect.Two && gameObject.transform.rotation.x <= 108 && gameObject.transform.rotation.x >= 72 || gameObject.transform.rotation.x <= -252 && gameObject.transform.rotation.x >= -288)
         {
             spinnerTwo = true;
         }
-        if (codeNumber == CodeCorrect.Three && gameObject.transform.rotation.x <= 50 && gameObject.transform.rotation.x >= 75)
+        else if (codeNumber == CodeCorrect.Three && gameObject.transform.rotation.x <= -180 && gameObject.transform.rotation.x >= -216 || gameObject.transform.rotation.x <= 36 && gameObject.transform.rotation.x >= 0)
         {
             spinnerThree = true;
         }
-        if (codeNumber == CodeCorrect.Four && gameObject.transform.rotation.x <= 50 && gameObject.transform.rotation.x >= 75)
+        else if (codeNumber == CodeCorrect.Four && gameObject.transform.rotation.x <= -324 && gameObject.transform.rotation.x >= -360 || gameObject.transform.rotation.x <= 180 && gameObject.transform.rotation.x >= 144)
         {
             spinnerFour = true;
         }
