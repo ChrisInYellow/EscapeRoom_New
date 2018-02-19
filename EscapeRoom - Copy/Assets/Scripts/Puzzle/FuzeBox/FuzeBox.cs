ec4a216bc523a58010cd9a231acabb3a2cabd574
trustred <trust870@hotmail.com>
2018-02-19 14:55:11 +0100

    cmt

--- EscapeRoom - Copy/Assets/Scripts/Puzzle/FuzeBox/FuzeBox.cs
+++ EscapeRoom - Copy/Assets/Scripts/Puzzle/FuzeBox/FuzeBox.cs
@@ -5,10 +5,6 @@ using UnityEngine.Events;
 
 public class FuzeBox : MonoBehaviour 
 {
-    public System.Action lightOn;
-    public System.Action lightOff;
-
-    public bool isWallTileRelated = false;
 
     public UnityEvent fuzeInserted = new UnityEvent();
     public UnityEvent fuzeRemoved = new UnityEvent();
@@ -27,10 +23,6 @@ public class FuzeBox : MonoBehaviour
         timeUntilRemoved = Random.Range(30, 60);
         thrust = Random.Range(150, 200);
         Invoke("ShootOutFuze", timeUntilRemoved);
-        if (isWallTileRelated)
-        {
-            lightOn();
-        }
     }
 
     public void ShootOutFuze ()
@@ -40,10 +32,6 @@ public class FuzeBox : MonoBehaviour
         Rigidbody rb = fuze.GetComponent<Rigidbody>();
         rb.isKinematic = false;
         rb.AddForce((-transform.right) * thrust);
-        if (isWallTileRelated)
-        {
-            lightOff();
-        }
     }
 
     public void OnUnSnapped()
@@ -51,9 +39,5 @@ public class FuzeBox : MonoBehaviour
         CancelInvoke();
         fuzeIsSnapped = false;
         fuzeRemoved.Invoke();
-        if (isWallTileRelated)
-        {
-            lightOff();
-        }
     }
 }
