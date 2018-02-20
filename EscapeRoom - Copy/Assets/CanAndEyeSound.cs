fdf26bf29dea412d5ba5d5e41f3b99f7e4e94a9b
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2018-02-20 12:53:59 +0100

    Soundsss

---
+++ EscapeRoom - Copy/Assets/CanAndEyeSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CanAndEyeSound : MonoBehaviour {
+
+    public void Eye()
+    { 
+        FindObjectOfType<AudioManager>().Play("EyeSquish");  
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(transform.tag == "Can") {
+            if (Math.Abs(GetComponent<Rigidbody>().velocity.y) >= 0.3 || Math.Abs(GetComponent<Rigidbody>().velocity.x) >= 0.3
+                || Math.Abs(GetComponent<Rigidbody>().velocity.z) >= 0.3)
+            {
+                FindObjectOfType<AudioManager>().Play("CanDrop");
+            }
+        }
+    }
+}
