c787c07dab452d69324313772c849aa2ba8d73bb
Katomic88 <31647167+Katomic88@users.noreply.github.com>
2018-02-20 17:02:23 +0100

    drawer animation done for top drawer rest done soon.
    see title

---
+++ EscapeRoom - Copy/Assets/Scripts/FileDrawerAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileDrawerAnimation : MonoBehaviour {
+
+
+    public Animator anim;
+
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+
+    public void FileAnimOpen()
+    {
+        if (!anim.GetBool("Open"))
+        {
+            anim.SetBool("Open", true);
+        }
+        else
+        {
+            anim.SetBool("Open", false); 
+        }
+        
+    }
+}
