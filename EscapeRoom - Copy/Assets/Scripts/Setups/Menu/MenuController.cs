50cbee700ae71b040a5048450ceb88db0343950b
Hugo Cremer <slasken49@gmail.com>
2018-02-22 16:20:38 +0100

    Commit

--- EscapeRoom - Copy/Assets/Scripts/Setups/Menu/MenuController.cs
+++ EscapeRoom - Copy/Assets/Scripts/Setups/Menu/MenuController.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-<<<<<<< HEAD
-=======
-//using UnityEditor;
->>>>>>> b07475ab5f3f8a1c368c540e6532ce31ac5ccdbc
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
@@ -20,19 +16,8 @@ public class MenuController : MonoBehaviour
         SceneManager.LoadScene(allScenes[whichSceneToLoad]);
     }
 
-    public void ExitGame()
+    public void ExitGame ()
     {
-<<<<<<< HEAD
         Application.Quit();
-=======
-        if (Application.isEditor)
-        {
-           // EditorApplication.isPlaying = false;
-        }
-        else
-        {
-            Application.Quit();
-        }
->>>>>>> b07475ab5f3f8a1c368c540e6532ce31ac5ccdbc
     }
 }
