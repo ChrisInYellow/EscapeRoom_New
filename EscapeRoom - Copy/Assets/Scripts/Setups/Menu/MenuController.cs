8303772e3f20c904b8f2c8561f403b4fbb8de5d6
trustred <trust870@hotmail.com>
2018-02-22 16:05:47 +0100

    awd

--- EscapeRoom - Copy/Assets/Scripts/Setups/Menu/MenuController.cs
+++ EscapeRoom - Copy/Assets/Scripts/Setups/Menu/MenuController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
@@ -17,15 +16,8 @@ public class MenuController : MonoBehaviour
         SceneManager.LoadScene(allScenes[whichSceneToLoad]);
     }
 
-    public void ExitGame ()
+    public void ExitGame()
     {
-        if (Application.isEditor)
-        {
-            EditorApplication.isPlaying = false;
-        }
-        else
-        {
-            Application.Quit();
-        }
+        Application.Quit();
     }
 }
