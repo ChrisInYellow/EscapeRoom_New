e30883ea7fc83b34f1691ad5f7cd76b9324d5eb0
Hugo Cremer <slasken49@gmail.com>
2018-02-26 15:13:55 +0100

    Credits

---
+++ EscapeRoom - Copy/Assets/Scripts/Setups/Credits/StartCreditsScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartCreditsScene : MonoBehaviour 
+{
+    public string whichSceneToLoad;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        LoadScene(); 
+    }
+
+    public void LoadScene ()
+    {
+        SceneManager.LoadScene(whichSceneToLoad);
+    }
+}
