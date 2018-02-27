869b29a741abf67e72b910d68278fcd075255d36
trustred <trust870@hotmail.com>
2018-02-27 15:16:45 +0100

    Revert "asd"
    This reverts commit 37bdbd8f82a438f7dd21364320fd402c550e1761.

---
+++ EscapeRoom - Copy/Assets/DoorSlamScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoorSlamScript : MonoBehaviour {
+
+    public UnityEvent DoorSlamed = new UnityEvent();
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+    public Transform player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        DoorSlamActivate();
+    }
+    public void DoorSlamActivate()
+    {
+        player.position = new Vector3(player.position.x, player.position.y, 5.2f);
+        leftDoor.GetComponent<Animator>().SetTrigger("DoorSlam"); ;
+        rightDoor.GetComponent<Animator>().SetTrigger("DoorSlam");
+        FindObjectOfType<AudioManager>().Play("DoorSlam");
+        DoorSlamed.Invoke();
+        Destroy(gameObject);
+    }
+}
