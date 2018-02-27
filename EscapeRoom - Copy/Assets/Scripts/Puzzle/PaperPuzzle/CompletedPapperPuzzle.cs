c1f44d79a7295ec9a8521f403b570393c776bbe1
Hugo Cremer <slasken49@gmail.com>
2018-02-27 10:03:28 +0100

    A bunch of sound changes

--- EscapeRoom - Copy/Assets/Scripts/Puzzle/PaperPuzzle/CompletedPapperPuzzle.cs
+++ EscapeRoom - Copy/Assets/Scripts/Puzzle/PaperPuzzle/CompletedPapperPuzzle.cs
@@ -3,16 +3,28 @@ using UnityEngine.Events;
 
 public class CompletedPapperPuzzle : MonoBehaviour
 {
+    AudioSource source;
+
+    public AudioClip papperPlaced;
+    public AudioClip pling;
+
     [SerializeField]
     public UnityEvent puzzleSolved = new UnityEvent();
-    public AudioClip solveSound;
     [HideInInspector]
     public float numberOfPlacedPappers = 0;
 
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
     public void IncreasePlacedPappers ()
     {
-        if (GetComponent<AudioSource>() != null)
-            GetComponent<AudioSource>().Play();
+        if (source != null)
+        {
+            source.clip = papperPlaced;
+            source.Play();
+        }
         numberOfPlacedPappers += 1;
         if (numberOfPlacedPappers == 5)
         {
@@ -23,10 +35,10 @@ public class CompletedPapperPuzzle : MonoBehaviour
     public void PuzzleSolved()
     {
         puzzleSolved.Invoke();
-        if (GetComponent<AudioSource>() != null)
+        if (source != null)
         {
-            GetComponent<AudioSource>().clip = solveSound;
-            GetComponent<AudioSource>().Play();
+            source.clip = pling;
+            source.Play();
         }
     }
 }
